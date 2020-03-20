f0fee179ce9a0f19f62e70f3f069d4f076a6558d
HaruGakka <ffdd270@gmail.com>
2020-03-20 11:33:36 +0900

    Add Run Config,  Add Test.

---
+++ tests/how_can_use_result.rs
@@ -0,0 +1,26 @@
+
+
+
+#[cfg(test)]
+mod tests
+{
+    #[test]
+    fn test_add( )
+    {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test_match()
+    {
+        let number = 42;
+
+        let answer = match number
+        {
+            42 => "All Answer of Universe",
+            _ => "Nope."
+        };
+
+        assert_eq!( answer, "All Answer of Universe" );
+    }
+}
